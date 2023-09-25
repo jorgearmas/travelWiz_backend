5b49316f2f2e6bdd7a80cd5e05a97d904bf5983d
Jorge <jorgearmas49@gmail.com>
2023-09-25 12:14:14 -0600

    (Refactor-name) Interface gets predifined JPA methods to operate CRUD for entity Anuncio

--- src/main/java/com/travelWiz/repository/Anuncio.java
+++
@@ -1,9 +0,0 @@
-package com.travelWiz.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("anuncio")
-public interface Anuncio extends JpaRepository<Anuncio, Integer>{
-
-}
