9b7e62de22eb4cab46860a4155cdb47deddf6dd7
Jorge <jorgearmas49@gmail.com>
2023-09-25 09:45:08 -0600

    First commit

---
+++ src/main/java/com/travelWiz/TravelWizApplication.java
@@ -0,0 +1,13 @@
+package com.travelWiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelWizApplication.class, args);
+	}
+
+}
