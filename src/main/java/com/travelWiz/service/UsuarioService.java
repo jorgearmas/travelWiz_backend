8b233cdde24bfff0dab2b39902967fb4d765919d
Jorge <jorgearmas49@gmail.com>
2023-09-25 19:15:03 -0600

    Class defines CRUD services and paths for entity Usuario via JPA rep

---
+++ src/main/java/com/travelWiz/service/UsuarioService.java
@@ -0,0 +1,43 @@
+package com.travelWiz.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.travelWiz.entity.Usuario;
+import com.travelWiz.repository.UsuarioRepository;
+
+
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin
+public class UsuarioService {
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	@GetMapping(path = "/buscar")
+	public List<Usuario> buscar(){
+		return usuarioRepository.findAll();
+	}
+	
+	@PostMapping(path = "/guardar")
+	public Usuario guardar(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	@DeleteMapping(path="/eliminar/{correo}")
+	public void eliminar(@PathVariable("correo") String correo){
+		usuarioRepository.deleteById(correo);	
+	}
+}
